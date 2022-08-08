f370dc9a1264f299f60575d48d37a8c108bf4266
Deepesh Zagade <zagadedeepesh@gmail.com>
2022-08-08 19:07:06 +0530

    Introduced Next-previous button, and loading spinner also

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner'
 
 export class News extends Component {
 
@@ -8,31 +9,64 @@ export class News extends Component {
         super()
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=aed587c050be4bbbb70d7d31c1fc6331"
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=aed587c050be4bbbb70d7d31c1fc6331&page=1&pageSize=${this.props.pagesize}`
+        let data = await fetch(url)
+        this.setState({loading: true})
+        let parseddata = await data.json()
+        this.setState({articles: parseddata.articles,
+                       totalresults: parseddata.totalResults,
+                       loading: false})
+    }
+    // Function to go on next page -changed the state over here.
+    nextPage =async ()=>{
+        
+        this.setState({loading: true})
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=aed587c050be4bbbb70d7d31c1fc6331&page=${this.state.page+1}&pageSize=${this.props.pagesize}`
         let data = await fetch(url)
         let parseddata = await data.json()
-        this.setState({articles: parseddata.articles})
-        
+        this.setState({
+            page: this.state.page +1,
+            articles: parseddata.articles,
+            loading: false
+        })
+    }
+    
+    // Function to go on previous page -changed the state over here.
+    previousPage =async ()=>{
+        this.setState({loading: true})
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=aed587c050be4bbbb70d7d31c1fc6331&page=${this.state.page-1}&pageSize=${this.props.pagesize}`
+        let data = await fetch(url)
+        let parseddata = await data.json()
+        this.setState({
+            page: this.state.page -1,
+            articles: parseddata.articles,
+            loading: false
+        })
     }
 
     render() {
         return (
             <div>
                 <div className="container my-3">
-                    <h1 className='my-3'>News Today - Fastest News Network</h1>
-                    <div className="row my-3">
-                        {this.state.articles.map((element)=>{
+                    <h1 className='my-3 text-center'>News Today - Fastest News Network</h1>
+                    {this.state.loading&&<Spinner />}
+                    <div className="row my-3">  
+                        {!this.state.loading&&this.state.articles.map((element)=>{
                             return <div className="col-md-4 my-3" key={element.url} >
                             <Newsitem title={element.title?element.title:""} description={element.description?element.description:""} imgurl={element.urlToImage?element.urlToImage:"https://images.news18.com/ibnlive/uploads/2022/08/untitled-design-2022-08-06t220634.879-165980389216x9.jpg"} 
                             newsurl = {element.url} />
                         </div>
                         })}
-                        
+                    </div>
+                    <div className="container d-flex justify-content-between">
+                    <button type="button" onClick={this.previousPage} disabled={this.state.page===1} className="btn btn-dark">&larr; Previous</button>
+                    <button type="button" onClick={this.nextPage} disabled={this.state.page+1>Math.ceil(this.state.totalresults/this.props.pagesize)} className="btn btn-dark">Next &rarr;</button>
                     </div>
                 </div>
             </div>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -10,7 +10,7 @@ export class Newsitem extends Component {
                         <div className="card-body">
                             <h5 className="card-title">{title.slice(0,45)}</h5>
                             <p className="card-text">{description.slice(0,88)}...</p>
-                            <a href={newsurl} target = "_blank" className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsurl} target = "_blank" className="btn btn-sm btn-dark">Read More</a>
                         </div>
                 </div>
             </div>
